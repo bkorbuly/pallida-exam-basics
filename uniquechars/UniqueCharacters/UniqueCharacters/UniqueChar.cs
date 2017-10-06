e7533a0dafe8dbadd963a78e1c8034806588b0ff
bkorbuly <b.korbuly@gmail.com>
2017-10-06 10:27:14 +0200

    Uniqcharacter function added and tested manually

---
+++ uniquechars/UniqueCharacters/UniqueCharacters/UniqueChar.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniqueCharacter
+{
+    public class UniqueChar
+    {   
+        static public List<char> UniqCharacters(string text)
+        {
+            var uniqcharlist = new List<char>();
+            int counter = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                counter = 0;
+                for (int j = 0; j < text.Length; j++)
+                {
+                    if(text[i] == text[j])
+                    {
+                        counter++;
+                    }
+                }
+                if(counter ==1)
+                {
+                    uniqcharlist.Add(text[i]);
+                }
+            }
+            return uniqcharlist;
+        }
+    }
+}

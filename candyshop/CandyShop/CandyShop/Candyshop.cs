f91247806185d303e0bf04ad3b352a5fd687cc40
bkorbuly <b.korbuly@gmail.com>
2017-10-06 12:16:14 +0200

    Candies and Lollipopes creating functions added

--- candyshop/CandyShop/CandyShop/Candyshop.cs
+++ candyshop/CandyShop/CandyShop/Candyshop.cs
@@ -11,10 +11,27 @@ namespace CandyShop
         int sugar;
         int candies;
         int lollipops;
+        List<Sweet> sweets = new List<Sweet>();
 
         public Candyshop(int sugar)
         {
             this.sugar = sugar;
         }
+
+        public void Createsweets(Candie candie)
+        {
+            sweets.Add(candie);
+            sugar -= candie.sugarNeed;
+            ++candies;
+        }
+
+        public void Createsweets(Lollipop lollipop)
+        {
+            sweets.Add(lollipop);
+            sugar -= lollipop.sugarNeed;
+            ++lollipops;
+        }
+
+
     }
 }

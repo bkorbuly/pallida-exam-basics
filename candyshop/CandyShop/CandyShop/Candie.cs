9921c7843ee2655d84011738d408d5817923ed2b
bkorbuly <b.korbuly@gmail.com>
2017-10-06 12:05:08 +0200

    Sweet parent class, candie and lollipopo child classes added

---
+++ candyshop/CandyShop/CandyShop/Candie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandyShop
+{
+    class Candie : Sweet
+    {
+        public Candie()
+        {
+            this.price = 20;
+            this.sugarNeed = 10;
+        }
+    }
+}

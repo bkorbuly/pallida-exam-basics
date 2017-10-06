e155641e6b82bbcd5cf9a8fe67df011c570fd127
bkorbuly <b.korbuly@gmail.com>
2017-10-06 11:23:18 +0200

    favouriteanimals arg parameter check function added and write to file function added

---
+++ favouriteanimals/FavouriteAnimals/FavouriteAnimals/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FavouriteAnimals
+{
+    class Program
+    {
+        static string filename = @"../../../../favourites.txt";
+        static void Main(string[] args)
+        {
+            ArgsEmptyorNot(args);
+            Console.ReadLine();
+        }
+
+        public static void WriteToFile(string[] args)
+        {
+            using (var write = new StreamWriter(filename))
+            {
+                for (int i = 0; i < args.Length; i++)
+                {
+                    write.WriteLine(args[i]);
+                    Console.WriteLine(args[i]);
+                }
+            }
+        }
+
+        public static void ArgsEmptyorNot(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("C# FavouriteAnimals [animal] [animal]");
+            }
+            else
+            {
+                WriteToFile(args);
+            }
+        }
+    }
+}
